c2df491bdd5f1342b5286440148f256f1fa48c02
Алексей Марченков <eng48mar@gmail.com>
2024-12-11 19:01:40 +0400

    removing unused namespaces

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using System.Globalization;
-using System.Linq.Expressions;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MathEvaluation.Context;
 using MathEvaluation.Extensions;
 using MathEvaluation.Parameters;
